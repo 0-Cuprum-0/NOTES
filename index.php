95a85622ff8425c2df12fd05f396a8b4b93919d7
0-Cuprum-0 <yesochek@gmail.com>
2025-04-18 19:19:03 +0300

    dfrgthyj

--- index.php
+++ index.php
@@ -10,9 +10,28 @@
 <body>
     <?php
     session_start();
-    $_SESSION['reg'] = false;
+    $link = connect();
+    $_SESSION['reg'] = true;
     include_once('templates/user.php');
     include_once('templates/menu.php');
+
+
+    $sel = 'SELECT * FROM users
+        WHERE id=1 ';
+    $res = mysqli_query($link, $sel);
+    echo '<table >';
+    while ($row = mysqli_fetch_array($res, MYSQLI_NUM)) {
+        echo '<tr>';
+
+        $img = base64_encode("avatar");
+        echo '<td><img height="100px" src="data:image/jpeg; base64,' . $img . '"></td>';
+    }
+    mysqli_free_result($res);
+    echo '</table>';
+
+
+
+
     #echo "HELLOWORLD!!"
     #
 
--- templates/user.php
+++ templates/user.php
@@ -1,24 +1,30 @@
 <?php
+$link = connect();
+session_start();
 #echo "HELLLO USER";
 if ($_SESSION['reg'] == true) {
-    $sel = 'SELECT name FROM users WHERE id = ' . $_SESSION['usr_id']; #IT MUST BE SET DURING REGISTRATION!!!;####ЭТО ДОЛЖНО РАБОТАТЬ
+    echo "QQQQ";
+    $sel = 'SELECT name FROM users WHERE id = ' . $_SESSION['usr_id']; #IT MUST BE SET DURING REGISTRATION!!!;####
     $rel = mysqli_query($link, $sel);                                 ###AND DB MUST NOT BE EMPTY
     echo $rel;
     echo '<div class="user"> <p>' . $rel . '<p></div>';
 } else {
     echo 'AGAIN?';
-    $sel = 'SELECT avatar FROM users WHERE id = 1 ';
-
-
-
-    echo 'aaaaaaaaa';
+    $sel = 'SELECT * FROM users  ';
     $res = mysqli_query($link, $sel);
+    echo $res;
+    if (mysqli_num_rows($res) > 0) {
+        while ($row = $res->fetch_assoc()) {
+            $sel = 'SELECT * FROM users ';
+            $rel = mysqli_query($link, $sel);
+            $imageData = base64_encode($row['avatar']);
+            echo $imageData;
+            echo '<img src="data:image/png;base64,' . base64_encode($row['avatar']) . '" alt="Avatar">';
+        };
+    } else {
+        echo "111";
+    }
 
-    while ($row = $res->fetch_assoc()) {
-        $imageData = base64_encode($row['avatar']);
-        echo $imageData;
-        echo '<img src="data:image/png;base64,' . $imageData . '" alt="Avatar">';
-    };
 
 
     // if ($res->num_rows > 0) {
