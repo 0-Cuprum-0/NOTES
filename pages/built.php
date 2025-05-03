aeff0c0a580f7abcd57e0941ab6ddef6db65e17f
0-Cuprum-0 <yesochek@gmail.com>
2025-05-03 22:14:32 +0300

    WOOORKS WITH TITLES

--- pages/built.php
+++ pages/built.php
@@ -1,22 +1,41 @@
 <?php
 $link = connect();
 $rel = 'SELECT * FROM pages WHERE user_id ="' . $_SESSION['id'] . '" ORDER BY crdate';
-$res = mysqli_query($link, $rel); #тут все норм       #
+$res = mysqli_query($link, $rel); #тут все норм  
+$another = 'SELECT COUNT(*) FROM pages WHERE user_id ="' . $_SESSION['id'] . '" ';
+$aa = mysqli_query($link, $another);   #
 $all = [];
-for ($i = $_SESSION['num']; $i > 0; $i++) {                                         #
+$s = mysqli_fetch_array($aa);
+
+$count = $s[0];
+for ($i = $count; $i > 0; $i--) {                                         #
     if ($row = mysqli_fetch_array($res, MYSQLI_NUM)) {    #
         array_push($all, $row);                           #
     }
-}                                                  #
-// echo '<pre>';                                      #                     
-print_r($all);                                     #
-// echo $res;                                         #
-// echo '</pre>';                                     #
-// while ($row = mysqli_fetch_assoc($res)) {  #<------#
+}
+$count = $s[0];
+for ($i = $count - 1; $i >= 0; $i--) {
+    $element = $all[$i][1];
+    print_r($element);
+    print_r("  ");
+}
+
+
+#
+// echo $count;                                      #                     
+                                    #
+// echo $aa;                                         #
+// echo '</pre>';    
+
+
+
+#
+// while ($row = mysqli_fetch_assoc($aa)) {  #<------#ПРОВЕРКА ВЫВОДОВ БД
 //     echo "<pre>";
 //     print_r($row);
 //     echo "</pre><hr>";
 // }
+
 
 // for ($i = $_SESSION['num']; $i > 0; $i++) {
 //     echo $all[$i]['page_name'];
