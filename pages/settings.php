bb384d280616b6037f49bcc4eda2771e09492445
0-Cuprum-0 <yesochek@gmail.com>
2025-05-11 17:33:18 +0300

    недоделыш

--- pages/settings.php
+++ pages/settings.php
@@ -151,24 +151,26 @@ if (!isset($_POST['logout'])) {
             <input type="hidden" name="logout" value="1">
             <button onclick="confirmm(event)" type="button" class="button mt-3">Log out</button>
         </div>
+    </form>
 
 
 
-    <?php
+<?php
     // echo $_SESSION['id'];
 }
+include_once("pages/tags_form.php");
 
 
 
-    ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.js"></script>
 
 
-    <style>
+<style>
 
-    </style>
+</style>
 </body>
 
 </html>
---
+++ pages/tags_form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tags form</title>
+</head>
+
+<body>
+    <h3 class="p-0 my-auto">Tags</h3>
+    <form action="index.php?page=3" class="p-0" id="myForm" method="POST">
+        <div class="form-group">
+            <input type="text" class="form-input my-3 w-50" name="pass">
+            <button type="button" class="button mt-3">Create</button>
+        </div>
+    </form>
+
+    <?php
+
+    ?>
+
+</body>
+
+</html>
